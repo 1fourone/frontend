b72fbb795f20456084fc8f29cd720270ead98247
Matias Lago <dev@1fourone.io>
2020-02-14 22:47:31 -0500

    Add initial files

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script to test the local file once
+
+# You test by moving it to ~/public_html/testing
+# Use naming of index.html but inside testing folder
+cp index.html ~/public_html/testing/
+cp styles.css ~/public_html/testing/
+
