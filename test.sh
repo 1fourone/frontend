e73d5bd9ac87ae74dd6cbdb7d68b22f018d1da69
Matias Lago <dev@1fourone.io>
2020-02-15 19:03:52 -0500

    Update remote testing script

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to test the local file once
-
-# You test by moving it to ~/public_html/testing
-# Use naming of index.html but inside testing folder
-cp index.html ~/public_html/testing/
-cp styles.css ~/public_html/testing/
-
