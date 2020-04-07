669bfabddd6289a829016aa795b96734f46db817
Matias Lago <dev@1fourone.io>
2020-04-06 23:34:21 -0400

    Finish basic grading functionality

--- mid/grader.php
+++ mid/grader.php
@@ -11,11 +11,18 @@
         fwrite($file, $q->{'studentInput'} . "\n");
         foreach($q->{'testCases'} as $tc) {
             fwrite($file, "print(" . $q->{'functionName'} . "(");
-            $args = explode(" ", $tc[0]);
-            for($i = 0; $i < count($args); $i++) {
-                fwrite($file, $args[$i]);
-                if($i != (count ($args) - 1))
-                    fwrite($file, ",");
+            
+            /* print arrays/tuples 'as is' */
+            if($tc[0][0] == "(" || $tc[0][0] == "[")
+                fwrite($file, $tc[0]);
+            else
+            {
+                $args = explode(" ", $tc[0]);
+                for($i = 0; $i < count($args); $i++) {
+                    fwrite($file, $args[$i]);
+                    if($i != (count ($args) - 1))
+                        fwrite($file, ",");
+                }
             }
             fwrite($file, "))\n");
         }
@@ -28,17 +35,54 @@
     */
     function evaluateQuestion($q) {
         populateSubmissionFile($q);
-        $output = explode("\n", shell_exec("python3.8 /tmp/submission.py 2>&1 "));
+        $outputString = shell_exec("python3.8 /tmp/submission.py 2>&1 ");
+        $outputLines = explode("\n", $outputString);
         $result = (object)[];
 
-        /* Populate the graderQuestionOutput here */
-        $conditions = 3 + count($q->{'expectedOutput'}->{'tests'});
-    
-        /* placeholder here, should loop through conditions and set fields accordingly */ 
-        $result->{'name'} = 0;
-        $result->{'constraint'} = 0;
-        $result->{'colon'} = 0;
-        $result->{'tests'} = [1, 1, 1];
+        /* Error handling */
+        $errors = array("Error", "Interrupt", "Traceback");
+        $errorFound = false;
+        foreach($errors as $err) {
+            if(strstr($outputString, $err))
+                $errorFound = true;
+        }
+
+        /* Evaluate test cases/output */
+        $testResults = array();
+        $numOfTests = count($q->{'expectedOutput'}->{'tests'});
+
+        if($errorFound == true) {
+            for($i = 0; $i < $numOfTests; $i++)
+                array_push($testResults, 1);
+        }
+        else {
+            //check tests
+            for($i = 0 ; $i < $numOfTests; $i++) {
+                $trueLine = ($q->{'constraint'} == "print") ? 2 * $i : $i;
+                if($outputLines[$trueLine] != $q->{'testCases'}[$i][1])
+                    array_push($testResults, 1);
+                else
+                    array_push($testResults, 0);
+            }
+        }
+        $result->{'tests'} = $testResults;
+
+        /* Evaluate remaining criteria */
+        if(!strstr($q->{'studentInput'}, $q->{'functionName'}))
+            $result->{'name'} = 1;
+        else 
+            $result->{'name'} = 0;
+
+        $studentLines = explode("\n", $q->{'studentInput'});
+        if($studentLines[0][strlen($studentLines[0])-1] != ":") 
+            $result->{'colon'} = 1;
+        else
+            $result->{'colon'} = 0;
+
+        if(!strstr($q->{'studentInput'}, $q->{'constraint'}))
+            $result->{'constraint'} = 1;
+        else
+            $result->{'constraint'} = 0;
 
         return json_encode($result);
     }
