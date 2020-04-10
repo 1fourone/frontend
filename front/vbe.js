6ae5cc1af92fae91aa66aa998b146ac577a83241
Matias Lago <dev@1fourone.io>
2020-04-10 15:05:15 -0400

    Add exam VBE

--- front/vbe.js
+++ front/vbe.js
@@ -65,7 +65,47 @@ function createQuestionListVBE(questions=[], isDraggable=true) {
     referenceNode.parentNode.insertBefore(wrapper, referenceNode.nextSibling);
 }
 
-function test() {
+/* question will be passed the transfer data from a drop event 
+    on drag, get a question object's id and store it as 
+    text transfer data.
+    on drop, use that id to appendChild to container
+*/
+function createExamVBE(qID=None) {
+    var wrapper = document.createElement("div");
+    wrapper.setAttribute("class", "exam");
+    wrapper.setAttribute("id", "e" + qID);
+
+    var table = document.createElement("table");
+    var tr = document.createElement("tr");
+
+    tr.appendChild(document.getElementById(qID));
+    var points = document.createElement("td");
+    var ta = document.createElement("input");
+    ta.setAttribute("class", "exam-question-points");
+    ta.setAttribute("maxlength", 3);
+    /* can access the points array by selecting by class name then .value */
+
+    points.appendChild(ta);
+    tr.appendChild(points);
+    table.appendChild(tr);
+
+    wrapper.appendChild(table);
+    return wrapper;
+}
+
+function createExamListVBE(qIDs=[]) {
+    var wrapper = document.createElement("div");
+    wrapper.setAttribute("class", "exam-list");
+
+    for(let i=0; i < qIDs.length; i++) {
+        wrapper.appendChild(createExamVBE(qIDs[i]));
+    }
+
+    var referenceNode = document.getElementById("placeholder");
+    referenceNode.parentNode.insertBefore(wrapper, referenceNode.nextSibling);
+}
+
+function test(type) {
     const questions = [
         {
         "id": "1231j2k3j1l2k3j",
@@ -109,5 +149,16 @@ function test() {
         }
     ]
 
-    createQuestionListVBE(questions, true);
+    switch (type) {
+        case 1:
+            createQuestionListVBE(questions, true);
+            break;
+        case 2:
+            qIDs = []
+            for(let i=0; i < questions.length; i++)
+                qIDs.push("q-" + i);
+            createExamListVBE(qIDs);
+        default:
+            break;
+    }
 }
