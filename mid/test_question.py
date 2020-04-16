d2c342a7820ed9314a18a82c0a90c855411a0072
Matias Lago <dev@1fourone.io>
2020-04-16 12:55:48 -0400

    Prepare structure to move AFS files

--- mid/test_question.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-def test(q):
-    r = requests.get('http://localhost:8080/grader.php?type=test&question=' +  json.dumps(q['data']))
-    reply = json.loads(r.text)
-    assert q['data']['expectedOutput'] == reply, 'Grader differs from expected output'
