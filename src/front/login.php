606b30efd307ec4b248904665ca05c309c5566e0
Matias Lago <dev@1fourone.io>
2020-04-19 09:35:19 -0400

    Add login files

---
+++ src/front/login.php
@@ -0,0 +1,22 @@
+<?php
+    //JSON Credentials is available via $_POST['credentials']
+    $cred = $_POST['credentials'];
+    $c = json_decode($cred);
+
+    //talk to mid.php via cURL and send a POST request with the (plaintext) credentials JSON string
+    //and receive a result JSON string
+    $ch = curl_init();
+    
+    curl_setopt($ch, CURLOPT_URL, "http://localhost/mid/login.php");
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "credentials=" . $cred);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    if($output === false)
+        echo 'Curl error: ' . curl_error($ch);
+    else
+        echo $output;
+?>
